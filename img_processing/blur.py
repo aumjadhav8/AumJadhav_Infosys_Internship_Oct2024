ec37bb4d921a08e4aa994a0d5936015f9e4792cf
Aum Jadhav <73793027+aumjadhav8@users.noreply.github.com>
2024-11-14 18:27:28 +0530

    Delete img_processing directory

--- img_processing/blur.py
+++
@@ -1,8 +0,0 @@
-import cv2
-
-img = cv2.imread("images\download.jpeg")
-blur = cv2.GaussianBlur(img, (15, 15), 0)
-
-cv2.imshow('Blurred Image', blur)
-cv2.waitKey(0)
-cv2.destroyAllWindows() 
--- img_processing/resize.py
+++
@@ -1,8 +0,0 @@
-import cv2
-
-img = cv2.imread('images\img.jpeg')
-resized = cv2.resize(img, (200, 200))
- 
-cv2.imshow('Resized Image', resized)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
